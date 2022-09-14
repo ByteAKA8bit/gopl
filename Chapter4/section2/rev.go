2bbeb672f9c604b016aa065914bd71fcb5a117f2
ZhaoJun <153286377@qq.com>
2022-09-14 18:17:55 +0800

    synchronization

---
+++ Chapter4/section2/rev.go
@@ -0,0 +1,19 @@
+package section2
+
+func Reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func Equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ Chapter4/section3/other/equal.go
@@ -0,0 +1,14 @@
+package section3
+
+func Equal(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
